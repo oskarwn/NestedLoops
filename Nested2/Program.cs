28186859a79c5a3c13b71d3118caaa05a284712b
Oskar Wihren <oskarwn@vgy.se>
2016-10-14 14:13:56 +0200

    Övn1

--- Nested2/Program.cs
+++ Nested2/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace Nested2
 {
-    class Program
-    {
-        static void Main(string[] args)
+         class Program
         {
+            static void Main(string[] args)
+            {   /*
+            //Övning 1 
+            for (int i = 0; i < 5; i++)
+            {
+                for (int inne = 0; inne < 5 ; inne++)
+                {
+                    Console.Write("o#");
+                }
+                Console.WriteLine();
+            }
+            */
+                //Övning 2
+                for (int i = 0; i < 11; i++)
+                {
+                    for (int inne = 0; inne < 11; inne++)
+                    {
+                        Console.Write("");
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
-}
+
+
