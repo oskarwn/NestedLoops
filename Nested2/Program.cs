0d3b907717f049d62687feb1ae6dc6db76ef9c4f
Oskar Wihren <oskarwn@vgy.se>
2016-10-14 15:14:06 +0200

    Klar med övn4

--- Nested2/Program.cs
+++ Nested2/Program.cs
@@ -19,18 +19,77 @@ namespace Nested2
                 }
                 Console.WriteLine();
             }
-            */
+            
                 //Övning 2
-                for (int i = 0; i < 11; i++)
+            for (int i = 0; i < 11; i++)
+            {
+                for (int j = 0; j < 11; j++)
                 {
-                    for (int inne = 0; inne < 11; inne++)
+                    if (i % 2 == 0)
+                        {       
+                            if (j%2==0)
+                            {
+                                Console.Write("o");
+                            }
+                            else
+                            {
+                                Console.Write("#");
+                            }
+                        }
+                    
+                     else {
+                            if (j % 2 == 0)
+                            {
+                                Console.Write("#");
+                            }
+                            else
+                            {
+                                Console.Write("o");
+
+                            }
+                        }
+                    }
+                    Console.WriteLine();
+            }
+                
+                //Övning 3
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 10; i < j; j--)
                     {
-                        Console.Write("");
+                        Console.Write("#");
                     }
                     Console.WriteLine();
                 }
+                
+                //Övning 4
+            for (int i = 0; i < 7; i++)
+            {
+                for (int inne = 0; inne < i; inne++)
+                {
+                    Console.Write(".");
+                }
+                for (int inne2 = 0; inne2 < (7-i); inne2++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
             }
-        }
+            */
+            //Övning 5
+            for (int i = 0; i < 7; i++)
+            {
+                for (int inne = 0; inne < 7; inne++)
+                {
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }    
     }
+}
+    
 
 
